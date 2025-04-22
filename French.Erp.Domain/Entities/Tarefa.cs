c0d87acf2a71c6896a3398f0c92f83c57e3d6007
Humberto Almeida <humberto.almeida@credpay.com.br>
2025-04-22 19:28:45 -0300

    ajustes psops columns

--- French.Erp.Domain/Entities/Tarefa.cs
+++ French.Erp.Domain/Entities/Tarefa.cs
@@ -22,30 +22,65 @@ namespace French.Erp.Domain.Entities
         {
             validationResult = new Validation.ValidationResult();
             isValid = false;
-            ComposicaoNotaFiscal = new HashSet<ComposicaoNotaFiscal>();
-            TarefaItem = new HashSet<TarefaItem>();
+            ComposicaoNotaFiscals = new List<ComposicaoNotaFiscal>();
+            TarefaItems = new List<TarefaItem>();
         }
 
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity), Required]
         public int TarefaId { get; set; }
+        
+        [Column, Required]
         public int ClienteId { get; set; }
+
+        [Column]
         public int? NotaFiscalId { get; set; }
+
+        [Required, Column, MaxLength(50)]
         public string Nome { get; set; }
+
+        [MaxLength(250), Column]
         public string Observacao { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
         public decimal? ValorOrcado { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
         public decimal? TotalHoras { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
         public decimal? ValorHora { get; set; }
-        public decimal? ValorCobrado { get; set; }
-        public decimal? ValorDesconto { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
         public decimal? ValorBruto { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
+        public decimal? ValorDesconto { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
+        public decimal? ValorCobrado { get; set; }
+
+        [Column(TypeName = "numeric(18, 2)")]
         public decimal? Comissao { get; set; }
-        public DateTime? DataInicio { get; set; }
-        public DateTime? DataFim { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateOnly? DataInicio { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateOnly? DataFim { get; set; }
+
+        [Column, Required]
         public int UsuarioId { get; set; }
 
+        [ForeignKey("ClienteId")]
+        [InverseProperty("Tarefas")]
         public virtual Cliente Cliente { get; set; }
-        public virtual ICollection<ComposicaoNotaFiscal> ComposicaoNotaFiscal { get; set; }
-        public virtual ICollection<TarefaItem> TarefaItem { get; set; }
+
+        [InverseProperty("Tarefa")]
+        public virtual IList<ComposicaoNotaFiscal> ComposicaoNotaFiscals { get; set; } 
+
+        [InverseProperty("Tarefa")]
+        public virtual IList<TarefaItem> TarefaItems { get; set; } 
+
 
         #region Dados de Validação
         public virtual Validation.ValidationResult ValidationResult => validationResult;
